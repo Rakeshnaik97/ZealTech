e6a6180746881b80ee2575c4543aea6d4647a0c7
Rakeshnaik97 <36966210+Rakeshnaik97@users.noreply.github.com>
2018-05-14 15:02:47 +0530

    Connecting With FrontEnd
    Front end Was successfully Connected

--- ZealTechBackEnd/src/main/java/com/niit/ZealTechBackEnd/Model/User.java
+++ ZealTechBackEnd/src/main/java/com/niit/ZealTechBackEnd/Model/User.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
